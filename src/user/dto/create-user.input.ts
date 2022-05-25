fa8c5528a6357bb9d2794c6e0fdf30e253d8f6e4
Crazyorchid <zhugeruntao@gmail.com>
2022-05-25 22:48:41 +0930

    Update create-user.input.ts
    写错了， 应该是verification

--- src/user/dto/create-user.input.ts
+++ src/user/dto/create-user.input.ts
@@ -1,10 +1,11 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsEmail } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
   @IsNotEmpty()
+  @IsEmail()
   email: string;
 
   @Field()
